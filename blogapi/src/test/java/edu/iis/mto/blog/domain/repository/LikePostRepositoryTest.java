69a9a0e9e2f3cf1b02a4cef70f4f56735e444456
Patryk Janowski <215745@edu.p.lodz.pl>
2020-06-03 23:47:34 +0200

    test findByUserAndPost added

---
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -0,0 +1,62 @@
+package edu.iis.mto.blog.domain.repository;
+
+import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class LikePostRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private LikePostRepository likePostRepository;
+
+    private User user;
+    private LikePost likePost;
+    private BlogPost blogPost;
+
+    @BeforeEach
+    void setUp() {
+        this.user = new User();
+        this.user.setFirstName("Jan");
+        this.user.setLastName("Walaszczyk");
+        this.user.setEmail("john@domain.com");
+        this.user.setAccountStatus(AccountStatus.NEW);
+
+        this.blogPost = new BlogPost();
+        this.blogPost.setEntry("Test");
+        this.blogPost.setUser(this.user);
+
+        this.likePost = new LikePost();
+        this.likePost.setPost(this.blogPost);
+        this.likePost.setUser(this.user);
+
+        this.entityManager.persist(this.user);
+        this.entityManager.persist(this.blogPost);
+    }
+
+    @Test
+    void findByUserAndPost() {
+        this.likePostRepository.save(this.likePost);
+        List<LikePost> likePosts = this.likePostRepository.findAll();
+        assertThat(likePosts, hasSize(1));
+    }
+}
