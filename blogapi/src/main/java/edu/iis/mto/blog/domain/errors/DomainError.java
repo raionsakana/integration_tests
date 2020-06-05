c29f7cbc8e0a0863221b9241488d7721bc36c3fa
Patryk Janowski <215745@edu.p.lodz.pl>
2020-06-05 16:46:13 +0200

    DomainError user_not_confirmed added

--- blogapi/src/main/java/edu/iis/mto/blog/domain/errors/DomainError.java
+++ blogapi/src/main/java/edu/iis/mto/blog/domain/errors/DomainError.java
@@ -7,6 +7,7 @@ public class DomainError extends RuntimeException {
     public static final String USER_NOT_FOUND = "unknown user";
     public static final String POST_NOT_FOUND = "unknown post";
     public static final String SELF_LIKE = "cannot like own post";
+    public static final String USER_NOT_CONFIRMED = "only confirmed user can set likes";
 
     public DomainError(String msg) {
         super(msg);
