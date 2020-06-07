77585d56965a19406d9fcfd00601e1c4b609a246
Patryk Janowski <215745@edu.p.lodz.pl>
2020-06-08 01:34:03 +0200

    test addPostByConfirmedUser added

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddPostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddPostTest.java
@@ -10,4 +10,21 @@ import io.restassured.http.ContentType;
 
 public class AddPostTest extends FunctionalTests {
 
+    private static final String CONFIRMED_API = "blog/user/1/post";
+
+    private JSONObject jsonObj = new JSONObject().put("entry", "post test AddPostTest");
+
+    @Test
+    public void addPostByConfirmedUser() {
+        given().accept(ContentType.JSON)
+            .header("Content-Type", "application/json;charset=UTF-8")
+            .body(jsonObj.toString())
+            .expect()
+            .log()
+            .all()
+            .statusCode(HttpStatus.SC_CREATED)
+            .when()
+            .post(CONFIRMED_API);
+    }
+
 }
--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -1,3 +1,4 @@
 --this script initiates db for h2 db (used in test profile)
-insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'john@domain.com', 'John', 'Steward')
-insert into user (account_status, email, first_name) values ('NEW', 'brian@domain.com', 'Brian')
+insert into user (id, account_status, email, first_name, last_name) values (1, 'CONFIRMED', 'john@domain.com', 'John', 'Steward')
+insert into user (id, account_status, email, first_name) values (2, 'NEW', 'brian@domain.com', 'Brian')
+insert into user (id, account_status, email, first_name) values (3, 'REMOVED', 'adam@domain.com', 'Adam')
