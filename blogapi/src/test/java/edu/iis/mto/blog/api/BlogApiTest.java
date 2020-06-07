fb2547a4f70abb3a2905a9c2e83d3a97375fe11c
Patryk Janowski <215745@edu.p.lodz.pl>
2020-06-08 00:05:06 +0200

    test postBlogUserShouldResponse409WhenDataIntegrityViolationException added

--- blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.ArgumentMatchers.any;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +12,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -38,6 +40,7 @@ public class BlogApiTest {
 
     private UserRequest user;
     private Long newUserId = 1L;
+    private String endpointBlogUser = "/blog/user";
 
     @Before
     public void setUp() {
@@ -53,11 +56,22 @@ public class BlogApiTest {
     public void postBlogUserShouldResponseWithStatusCreatedAndNewUserId() throws Exception {
         String content = writeJson(this.user);
 
-        this.mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON)
+        this.mvc.perform(post(this.endpointBlogUser).contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON)
             .content(content))
             .andExpect(status().isCreated())
-            .andExpect(content().string(writeJson(new Id(newUserId))));
+            .andExpect(content().string(writeJson(new Id(this.newUserId))));
+    }
+
+    @Test
+    public void postBlogUserShouldResponse409WhenDataIntegrityViolationException() throws Exception {
+        when(this.blogService.createUser(any())).thenThrow(DataIntegrityViolationException.class);
+        String content = writeJson(new UserRequest());
+
+        this.mvc.perform(post(this.endpointBlogUser).contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(content))
+            .andExpect(status().is(409));
     }
 
     private String writeJson(Object obj) throws JsonProcessingException {
