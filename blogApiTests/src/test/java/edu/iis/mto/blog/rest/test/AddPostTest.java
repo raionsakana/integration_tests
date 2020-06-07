0b9a574f26bf4394fec22aa9e6af3a672213c9fc
Patryk Janowski <215745@edu.p.lodz.pl>
2020-06-08 01:25:58 +0200

    AddPostTest class added

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddPostTest.java
@@ -0,0 +1,13 @@
+package edu.iis.mto.blog.rest.test;
+
+import static io.restassured.RestAssured.given;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.http.ContentType;
+
+public class AddPostTest extends FunctionalTests {
+
+}
